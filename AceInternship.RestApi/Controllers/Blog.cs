a426966102f78f42be44bfd4957e1d6843958136
Aung_Lin <linoscar724@gmail.com>
2024-06-06 13:20:49 +0630

    [Complete] API Template

---
+++ AceInternship.RestApi/Controllers/Blog.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AceInternship.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Blog : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult GetBlogs()
+        {
+            return Ok("GetBlogs");
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateBlogs()
+        {
+            return Ok("CreateBlogs");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBlogs()
+        {
+            return Ok("UpdateBlogs");
+        }
+        [HttpPatch]
+        public IActionResult PatchBlogs()
+        {
+            return Ok("PatchBlogs");
+        }
+
+        [HttpDelete]
+
+        public IActionResult DeleteBlogs()
+        {
+            return Ok("DeleteBlogs");
+        }
+    }
+}
