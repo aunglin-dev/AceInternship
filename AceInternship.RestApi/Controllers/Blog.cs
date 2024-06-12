8cb7a36bce8297da9a664701ae9ee4b2551aecd7
Aung_Lin <linoscar724@gmail.com>
2024-06-12 11:28:05 +0630

    Creating CRUD using ASP.NET Core and Dapper

--- AceInternship.RestApi/Controllers/Blog.cs
+++ AceInternship.RestApi/Controllers/Blog.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AceInternship.RestApi.Model;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace AceInternship.RestApi.Controllers
 {
@@ -7,24 +12,53 @@ namespace AceInternship.RestApi.Controllers
     [ApiController]
     public class Blog : ControllerBase
     {
-        [HttpGet]
 
+        private readonly SqlConnectionStringBuilder _stringBuilder = new SqlConnectionStringBuilder()
+        {
+            InitialCatalog = "InternDB",
+            UserID = "sa",
+            Password = "123456",
+            DataSource = "LAPTOP-P3IDALMC",
+        };
+
+        [HttpGet]
         public IActionResult GetBlogs()
         {
-            return Ok("GetBlogs");
+            using IDbConnection db = new SqlConnection(_stringBuilder.ConnectionString);
+           List<BlogModel> lst = db.Query<BlogModel>(Queries.SelectAll).ToList();
+            return Ok(lst);
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSpecificBlogs(int id)
+        {
+            using IDbConnection db = new SqlConnection(_stringBuilder.ConnectionString);
+            var item= db.Query<BlogModel>(Queries.SelectBlogByID, new {BlogId = id}).FirstOrDefault();
+            if(item == null) 
+                 return NotFound("Failed to Retrieve Specific Data");
+            return Ok(item);
+
         }
 
         [HttpPost]
 
-        public IActionResult CreateBlogs()
+        public IActionResult CreateBlogs(BlogModel blog)
         {
-            return Ok("CreateBlogs");
+            using IDbConnection db = new SqlConnection(_stringBuilder.ConnectionString);
+            int results = db.Execute(Queries.BlogCreate, blog);
+            string messages = results > 0 ? "Successfully Data Saved" : "Failed To Saved Data";
+            return Ok(messages);
         }
 
-        [HttpPut]
-        public IActionResult UpdateBlogs()
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlogs(BlogModel blog, int id)
         {
-            return Ok("UpdateBlogs");
+            blog .BlogId = id;  
+            using IDbConnection db = new SqlConnection(_stringBuilder.ConnectionString);
+            int results = db.Execute(Queries.BlogUpdate, blog);
+            string message= results > 0 ? "Successfully Data Updated" : "Failed To Updated Data";
+            return Ok(message);
         }
         [HttpPatch]
         public IActionResult PatchBlogs()
@@ -32,11 +66,39 @@ namespace AceInternship.RestApi.Controllers
             return Ok("PatchBlogs");
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
 
-        public IActionResult DeleteBlogs()
+        public IActionResult DeleteBlogs(int id)
         {
-            return Ok("DeleteBlogs");
+            using IDbConnection db =new SqlConnection(_stringBuilder.ConnectionString);
+            int result= db.Execute(Queries.BlogDelete, new { BlogId = id });
+            string message = result > 0 ? "Successfully Data Deleted" : "Failed To Deleted Data";
+            return Ok(message);
         }
+    }
+
+    public static class Queries
+    {
+        public static string SelectAll { get; } = "Select * from tbl_Blog";
+
+        public static string SelectBlogByID{ get; } = "Select * from tbl_Blog where BlogId = @BlogId";
+
+
+        public static string BlogCreate { get; } = @"INSERT INTO [dbo].[tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent)";
+
+        public static string BlogUpdate { get; } = @"UPDATE [dbo].[tbl_Blog]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent
+ WHERE BlogId = @BlogId";
+
+        public static string BlogDelete { get; } = "Delete from tbl_Blog where BlogId = @BlogId";
     }
 }
---
+++ AceInternship.RestApi/Model/BlogModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AceInternship.RestApi.Model
+{
+
+    [Table("tbl_Blog")]
+    public class BlogModel
+    {
+        public int BlogId { get; set; }
+        public String BlogTitle { get; set; }
+        public String BlogAuthor { get; set; }
+        public String BlogContent { get; set; }
+
+    }
+}
